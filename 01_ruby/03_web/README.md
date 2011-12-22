6bad134a7cac3622097fbada8ecfc94e07454a79
Sunny Gleason <sunny.gleason@gmail.com>
2011-12-22 12:00:20 -0500

    adding web service example

---
+++ 01_ruby/03_web/README.md
@@ -0,0 +1,17 @@
+
+Calling Web Services using open-uri
+
+* Run the script using "ruby duck.rb"
+
+You should study the code for duck.rb and get to know it well.
+
+
+Next Steps:
+
+* Modify duck.rb to take the query as a command-line argument (or fail if it's not specified)
+* Copy & paste duck.rb to create duckdef.rb, modify duckdef.rb so that it just prints the definition from duck-duck-go results
+
+Advanced:
+
+* Study the Twitter search api here: https://dev.twitter.com/docs/api/1/get/search
+* Copy/Paste/Modify duck.rb to create twitter.rb that searches twitter for a command-line query and displays results
---
+++ 01_ruby/03_web/duck.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+require 'json'
+require 'pp'
+
+
+BASE_URL = "http://api.duckduckgo.com/?format=json&pretty=1&q="     # remote API url
+query     = "web services"                                          # query string
+query_url = BASE_URL + URI.escape(query)                            # putting the 2 together
+
+
+puts " ======================================== "                   # fancy output
+puts "   #{query_url}"                                              # fancy output
+
+object = open(query_url) do |v|                                     # call the remote API
+  input = v.read                                                    # read the full response
+#  puts input                                                       # un-comment this to see the returned JSON magic
+  JSON.parse(input)                                                 # parse the JSON & return it from the block
+end
+
+puts " ======================================== "                   # fancy output
+puts "   #{object['Heading']}"
+puts "     #{object['Abstract']}"
+puts "     #{object['AbstractURL']}"
+puts " ---------------------------------------- "
+
+object['RelatedTopics'].each do |rt|                                # processing sub-elements
+  puts
+  puts "   * #{rt['Text']}"
+  puts "     #{rt['FirstURL']}"
+end
+
+puts                                                                # fancy output
+puts " ---------------------------------------- "
+puts " ======================================== "
